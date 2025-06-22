606319da279e992e84fce38a92dc44f5e718a8c5
rafaelmoralesd <149271071+rafaelmoralesd@users.noreply.github.com>
2025-06-22 00:14:31 -0600

    Ejercicio 3

--- Tarea1.2.py
+++ Tarea1.2.py
@@ -16,10 +16,9 @@ df_productos_por_categoria = df_ventas.groupby("Producto")["Cantidad"].sum().res
 print("Cantidad total de productos vendidos por categoría:")
 print(df_productos_por_categoria)
 
+print("---------------------------------------------------------------------------")
 
 #EJERCICIO 2
-
-
 clima = pd.read_csv('clima.csv')
 
 print(clima.head())
@@ -48,6 +47,43 @@ print(f"Cantidad  de veces con temperatura mayor 30°C registrados: {len(calor_m
 dias_por_ciudad = clima['Ciudad'].value_counts()
 
 print(dias_por_ciudad)
+
+print("---------------------------------------------------------------------------")
+
+#EJERCICIO 3
+
+calificaciones = pd.read_csv("calificaciones.csv")
+
+print("Columnas:", calificaciones.columns)
+
+pivot = calificaciones.pivot_table(index='Estudiante', columns='Materia', values='Calificación')
+
+print("\nTabla de calificaciones por estudiante en el dataset:")
+print(pivot.head())
+
+promedio_materias = pivot.mean()
+print("\n Promedios por materia  en el dataset:")
+print(promedio_materias)
+
+materia_top = promedio_materias.idxmax()
+print(f"\nMateria con mayor promedio: {materia_top}")
+
+
+pivot['Promedio'] = pivot.mean(axis=1)
+
+
+destacados = pivot[pivot['Promedio'] > 85]
+print("\n Estudiantes con promedio mayor a 85:")
+print(destacados[['Promedio']])
+
+reprobados_por_materia = (pivot < 60).sum()
+materia_mas_reprobada = reprobados_por_materia.idxmax()
+print(f"\n Materia con más reprobados en el dataset: {materia_mas_reprobada}")
+
+peores = pivot.sort_values(by='Promedio').head(5)
+print("\n 5 estudiantes con menor promedio:")
+print(peores[['Promedio']])
+
 
 
 
